9043310a4aa6c3cecbe8192a9054afb301745646
Drazen <danstad2012@gmail.com>
2019-03-20 15:14:27 -0700

    start defining

---
+++ pplplacer.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+
+
+
+
+#create token to represent individual characters
+class token(object):
+    def character():
+        pass
+    def history():
+        pass
+    
+#create map to 
+class space(object):
+    def space():
+        pass
+
+class time(object):
+    def timeline():
+        pass
+
+
+def main():
+    pass
